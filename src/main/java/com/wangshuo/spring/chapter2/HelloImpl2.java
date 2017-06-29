4dedfd57b240df5696a3667a1b367032dfecb39e
wangshuo <wangshuo@wondersgroup.com>
2017-06-29 17:27:06 +0800

    chapter1.2

---
+++ src/main/java/com/wangshuo/spring/chapter2/HelloImpl2.java
@@ -0,0 +1,18 @@
+package com.wangshuo.spring.chapter2;
+
+import com.wangshuo.spring.chapter2.Helloworld.HelloApi;
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+public class HelloImpl2 implements HelloApi {
+    private String message;
+    public HelloImpl2(){
+        this.message="Hello nihao";
+    }
+    public HelloImpl2(String message){
+        this.message=message;
+    }
+    public void sayHello() {
+        System.out.println(message);
+    }
+}
---
+++ src/test/java/chapter2/InstantiatingBeanTest.java
@@ -0,0 +1,8 @@
+package chapter2;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+public class InstantiatingBeanTest {
+
+}
