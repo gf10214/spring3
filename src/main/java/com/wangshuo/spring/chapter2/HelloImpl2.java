4cccf658f3ecc4a29fafd8e55eb0fd316df2da0c
youzai <youzai@pinduoduo.com>
2018-06-05 20:39:38 +0800

    333333

--- src/main/java/com/wangshuo/spring/chapter2/HelloImpl2.java
+++ src/main/java/com/wangshuo/spring/chapter2/HelloImpl2.java
@@ -5,6 +5,7 @@ import com.wangshuo.spring.chapter2.Helloworld.HelloApi;
  * Created by Administrator on 2017/6/29.
  */
 public class HelloImpl2 implements HelloApi {
+
     private String message;
     public HelloImpl2(){
         this.message="Hello nihao";
